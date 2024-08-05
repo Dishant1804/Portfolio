5a44583472cbd327d303b4a501fd0eced61a3af3
Dishant1804 <dishantmiyani1804@gmail.com>
2024-08-05 13:28:43 +0530

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,13 +20,6 @@ export default function RootLayout({
         <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
       </head>
       <body>
-        <Script
-          src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
-          strategy="afterInteractive"
-          integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
         <SmoothScroll>
           <Navbar />
           {children}
